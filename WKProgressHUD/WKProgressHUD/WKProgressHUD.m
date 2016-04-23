ab27bc7437978f5cc91b3148377d7977ba2e832a
WelkinXie <welkin995@126.com>
2016-04-23 20:02:44 +0800

    URL

--- WKProgressHUD/WKProgressHUD/WKProgressHUD.m
+++ WKProgressHUD/WKProgressHUD/WKProgressHUD.m
@@ -5,6 +5,8 @@
 //  Created by Welkin Xie on 3/8/16.
 //  Copyright © 2016 WelkinXie. All rights reserved.
 //
+//  Github: https://github.com/WelkinXie/WKProgressHUD
+//
 
 #import "WKProgressHUD.h"
 #import "WKMessageProgressHUD.h"
